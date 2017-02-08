41fc92ab552d41f4b1b84d4b3d5dd074973f40d6
tocausan <tocausan@gmail.com>
2017-02-08 04:25:26 +0100

    Container components

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { App } from './app/app';
+import { Main } from './app/containers/main';
 
 @NgModule({
     declarations: [
-        App
+        App,
+        Main
     ],
     imports: [BrowserModule],
     bootstrap: [App]
